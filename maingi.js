a27037e78c33dc1739c9eac509c0413c7bd3332a
Maingi765 <147986293+Maingi765@users.noreply.github.com>
2023-10-16 16:09:34 +0300

    Add files via upload

---
+++ maingi.js
@@ -0,0 +1,29 @@
+<!-- Academic Achievements section -->
+<section id="academic">
+    <h2>Academic Achievements</h2>
+    <p>Highlight your academic accomplishments, including degrees, honors, and research projects.</p>
+    <div class="read-more-content" style="display: none;">
+        <p>Additional details about your academic achievements go here.</p>
+    </div>
+    <button class="read-more-button" data-section-id="academic">Read More</button>
+</section>
+
+<!-- Career Highlights section -->
+<section id="career">
+    <h2>Career Highlights</h2>
+    <p>Describe your career achievements, work experiences, and any notable projects or positions.</p>
+    <div class="read-more-content" style="display: none;">
+        <p>Additional details about your career highlights go here.</p>
+    </div>
+    <button class="read-more-button" data-section-id="career">Read More</button>
+</section>
+
+<!-- Life Experiences section -->
+<section id="experiences">
+    <h2>Life Experiences</h2>
+    <p>Share significant personal experiences that have shaped your character and outlook on life.</p>
+    <div class="read-more-content" style="display: none;">
+        <p>Additional details about your life experiences go here.</p>
+    </div>
+    <button class="read-more-button" data-section-id="experiences">Read More</button>
+</section>
